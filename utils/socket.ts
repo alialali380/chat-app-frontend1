78a2f32692b5829788722906f49aa75c00218a36
Helal164 <helalalmohamed350@gmail.com>
2025-06-20 02:36:06 +0300

    first commit

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 
-const SOCKET_URL = 'http://192.168.1.109:3001'; // عنوان السيرفر
+const SOCKET_URL = 'https://chat-app-backend1-1.onrender.com/'; 
 
 export const socket: Socket = io(SOCKET_URL, {
   reconnection: true,
